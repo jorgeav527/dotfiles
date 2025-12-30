7c3a044d57b056ea6c89660461a6968ad1df2b95
jorge alarcon <jorgeav527@gmail.com>
2025-12-29 22:04:45 -0500

    add: soft wrap and color column

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -41,3 +41,8 @@ vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (defau
 vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+vim.opt.colorcolumn = '120' -- Draw a vertical ruler at column 120 as a visual line-length guide
+vim.opt.wrap = true -- Enable soft wrapping when lines exceed the window width
+vim.opt.linebreak = true -- Wrap lines at word boundaries instead of breaking words
+vim.opt.breakindent = true -- Keep indentation on wrapped lines for better readability
+vim.opt.showbreak = '↪ ' -- Prefix wrapped lines with a visual indicator
